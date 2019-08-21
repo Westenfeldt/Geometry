bf5f9800f0e30d4a2705477d185813902f4fbac3
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-21 13:10:21 +0200

    Add project files.

---
+++ Geometry/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Geometry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du kan vælge at regne arealet af en Firkant, Ret vinklet trekant eller en cirkel ud");
+            Console.WriteLine("Firkant: F");
+            Console.WriteLine("Retvinklet trekant: R");
+            Console.WriteLine("Cirkel: C");
+
+            string valg = Console.ReadLine();
+
+            switch (valg.ToLower())
+            {
+                case "c":
+                    Console.WriteLine("Angiv radiussen for din cirkel");
+                    double radius = Convert.ToDouble(Console.ReadLine());
+
+                    // Beregning
+
+                    double areal = Math.PI * radius * radius;
+
+                    Console.WriteLine("Arealet af din cirkel er {0}.", areal);
+
+                    break;
+
+            }
+
+        }
+    }
+}
